a26ee60ca35c4a2e5042537ec7733c4a4e45d142
Charlotte <smit-charlotte@outlook.com>
2025-03-17 09:25:23 +0100

    update

--- js/main.js
+++ js/main.js
@@ -24,3 +24,55 @@ card2.addEventListener("mouseover", () => {
 card2.addEventListener("mouseout", () => {
     card2.style.boxShadow = "none"
 })
+
+// Box shadow card 3
+
+let card3 = card[2]
+console.log(card3)
+
+card3.addEventListener("mouseover", () => {
+    card3.style.boxShadow = "rgb(119, 119, 119) 4px 4px 2px"
+})
+
+card3.addEventListener("mouseout", () => {
+    card3.style.boxShadow = "none"
+})
+
+// Box shadow card 4
+
+let card4 = card[3]
+console.log(card4)
+
+card4.addEventListener("mouseover", () => {
+    card4.style.boxShadow = "rgb(119, 119, 119) 4px 4px 2px"
+})
+
+card4.addEventListener("mouseout", () => {
+    card4.style.boxShadow = "none"
+})
+
+// Box shadow card 5
+
+let card5 = card[4]
+console.log(card5)
+
+card5.addEventListener("mouseover", () => {
+    card5.style.boxShadow = "rgb(119, 119, 119) 4px 4px 2px"
+})
+
+card5.addEventListener("mouseout", () => {
+    card5.style.boxShadow = "none"
+})
+
+// Box shadow card 6
+
+let card6 = card[5]
+console.log(card6)
+
+card6.addEventListener("mouseover", () => {
+    card6.style.boxShadow = "rgb(119, 119, 119) 4px 4px 2px"
+})
+
+card6.addEventListener("mouseout", () => {
+    card6.style.boxShadow = "none"
+})
