c25e8609f847748077b19fbb8390dee03a863c90
Charlotte <smit-charlotte@outlook.com>
2025-03-17 09:19:46 +0100

    update

--- js/main.js
+++ js/main.js
@@ -1,12 +1,26 @@
 let card = document.querySelectorAll(".card")
 
+// Box shadow card 1
 let card1 = card[0]
 console.log(card1)
 
 card1.addEventListener("mouseover", () => {
-    card1.style.boxShadow = " #000000 4px 4px"
+    card1.style.boxShadow = "rgb(119, 119, 119) 4px 4px 2px"
 })
 
 card1.addEventListener("mouseout", () => {
     card1.style.boxShadow = "none"
+})
+
+// Box shadow card 2
+
+let card2 = card[1]
+console.log(card2)
+
+card2.addEventListener("mouseover", () => {
+    card2.style.boxShadow = "rgb(119, 119, 119) 4px 4px 2px"
+})
+
+card2.addEventListener("mouseout", () => {
+    card2.style.boxShadow = "none"
 })
